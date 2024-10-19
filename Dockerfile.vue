10b909bbdc7fce4ecdffd392a030b9d25a8a18e4
daniell.gm03 <daniell.gm03@gmail.com>
2024-10-19 16:12:29 +0200

    initial project

---
+++ Dockerfile.vue
@@ -0,0 +1,36 @@
+# Usa una imagen base de Node.js Alpine
+FROM node:20.18-alpine
+
+# Establece el directorio de trabajo
+WORKDIR /var/www/html/frontend/billsdesk
+
+# Copia todos los archivos de billsdesk-vue
+COPY ./billsdesk-vue ./
+
+# RUN npm install
+
+# Expone el puerto para la aplicación
+EXPOSE 5173
+
+# CMD ["tail", "-f", "/dev/null"]
+CMD ["npm", "run", "install-and-run"]
+
+# # Usa una imagen base de Node.js Alpine
+# FROM node:20.18-alpine
+
+# # Establece el directorio de trabajo
+# WORKDIR /var/www/html/frontend/
+
+# # Copia todos los archivos de billsdesk-vue
+# COPY ./billsdesk-vue/ ./ 
+
+# # Cambia a la carpeta billsdesk y ejecuta npm install
+# # RUN npm install --prefix billsdesk
+
+
+# # Expone el puerto para la aplicación
+# EXPOSE 5173
+
+# # Comando para mantener el contenedor en ejecución
+# CMD ["tail", "-f", "/dev/null"]
+
